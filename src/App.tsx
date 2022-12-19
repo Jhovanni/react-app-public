9eba4480a884c66e8244f29b23f79a360cc82f3c
jhovanni <ejhova@gmail.com>
2022-12-18 21:07:01 -0600

    HashRouterDemo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/home/Home";
+import HashRouterDemo from "./components/hash-router/HashRouterDemo";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
+  { path: "/", element: <Home /> },
+  { path: "/hash-router/demo", element: <HashRouterDemo /> },
 ]);
 
 function App() {
