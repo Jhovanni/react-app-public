accdedd48a59788c3901c24d151ba810ec10345d
jhovanni <ejhova@gmail.com>
2022-12-18 21:00:50 -0600

    Prettier added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/home/Home";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>
-    }
+  {
+    path: "/",
+    element: <Home />,
+  },
 ]);
 
 function App() {
-
-
-    return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function Home() {
-    return <div
-        className="h-screen w-screen bg-pink-800 flex items-center justify-center text-white text-4xl font-semibold">Showcase
-        app</div>
+  return (
+    <div className="h-screen w-screen bg-pink-800 flex items-center justify-center text-white text-4xl font-semibold">
+      Showcase app
+    </div>
+  );
 }
